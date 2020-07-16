125c3e522040fc78c5e4285af1166c8eaad83070
frunoob <3024446880@qq.com>
2020-07-17 00:09:00 +0800

    it's my learn 2 day

--- app/src/main/java/com/frunoob/my_fist_app/DisplayMessageActivity.java
+++ app/src/main/java/com/frunoob/my_fist_app/DisplayMessageActivity.java
@@ -9,13 +9,5 @@ import static com.frunoob.my_fist_app.MainActivity.EXTRA_MESSAGE;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message);
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(EXTRA_MESSAGE);
-        TextView textView = findViewById(R.id.editText);
-        textView.setText(message);
-    }
+
 }
